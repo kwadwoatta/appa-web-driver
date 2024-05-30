f12f42c23ac2b5004788c48c110b566ca346a1c3
kwadwoatta <princeofori065@gmail.com>
2024-05-30 17:32:50 +0000

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 import { LoginComponent } from './components/login/login.component';
-import { PackageComponent } from './components/package/package.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'dashboard/package', component: PackageComponent },
 ];
